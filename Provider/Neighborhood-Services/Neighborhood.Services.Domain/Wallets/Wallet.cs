f6dcee9adc5b0f7240cef332b88d21def4b876ad
Ziad Gad <ziadmohamedgad@gmail.com>
2026-05-25 18:47:38 +0300

    wallets configuration implemented

--- Provider/Neighborhood-Services/Neighborhood.Services.Domain/Wallets/Wallet.cs
+++ Provider/Neighborhood-Services/Neighborhood.Services.Domain/Wallets/Wallet.cs
@@ -3,11 +3,13 @@ using Neighborhood.Services.Domain.Transactions;
 
 namespace Neighborhood.Services.Domain.Wallets
 {
-    public class Wallet : BaseEntity
+    public class Wallet : BaseEntity<int>
     {
         public string UserId { get; set; }
         public decimal Balance { get; set; }
-        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+        public ApplicationUser User { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
         public ICollection<Transaction> OutgoingTransactions { get; set; } = new HashSet<Transaction>();
         public ICollection<Transaction> IncomingTransactions { get; set; } = new HashSet<Transaction>();
     }
