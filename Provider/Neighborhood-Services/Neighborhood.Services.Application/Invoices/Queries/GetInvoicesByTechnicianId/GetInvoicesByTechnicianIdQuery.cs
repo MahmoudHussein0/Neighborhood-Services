6f391e618a39204b7ef2cb1f5e0dac03f97a5f63
Ziad Gad <ziadmohamedgad@gmail.com>
2026-05-31 12:52:25 +0300

    invoices dtos, commands, queries implemented

---
+++ Provider/Neighborhood-Services/Neighborhood.Services.Application/Invoices/Queries/GetInvoicesByTechnicianId/GetInvoicesByTechnicianIdQuery.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+using Neighborhood.Services.Application.Invoices.DTOs;
+namespace Neighborhood.Services.Application.Invoices.Queries.GetInvoicesByTechnicianId
+{
+    public class GetInvoicesByTechnicianIdQuery : IRequest<IEnumerable<InvoiceResponseDto>>
+    {
+        public int TechnicianId { get; set; }
+    }
+}
